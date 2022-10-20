6a2c8eb3de0ebd3e93c19eea26abe8322bebb06c
Void <arandomdude75@gmail.com>
2022-10-20 15:32:07 +0100

    fixed skill pages unable to load on github pages

--- js/index.js
+++ js/index.js
@@ -3,10 +3,10 @@
 //TODO: this needs cleaning
 function onSkillClicked(e)
 {
-  window.location = "../pages/"+skills[e.currentTarget.idx]+".html";
+  window.location.href = "pages/"+skills[e.currentTarget.idx]+".html";
 }
 
-const skills = ["programming","pixelart","music","editing"]
+const skills = ["programming","pixelart","music","editing"];
 for (let i = 0; i < skills.length; i++)
 {
   let skill = document.querySelector("#skills .skill-" + skills[i]);
@@ -22,8 +22,8 @@ function goToLink(e)
 
 let discord = document.querySelector("#discord");
 discord.link = "https://discordapp.com/users/776145967383969804";
-discord.addEventListener("click",goToLink,false)
+discord.addEventListener("click",goToLink,false);
 
 let itch = document.querySelector("#itch");
 itch.link = "https://x-void-x.itch.io";
-itch.addEventListener("click",goToLink,false)
+itch.addEventListener("click",goToLink,false);
