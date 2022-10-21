58d4d7506467babeb18f27214753f718c9e849d3
Void <arandomdude75@gmail.com>
2022-10-21 17:42:20 +0100

    added experimental creature that follow mouse

--- js/index.js
+++ js/index.js
@@ -27,3 +27,79 @@ discord.addEventListener("click",goToLink,false);
 let itch = document.querySelector("#itch");
 itch.link = "https://x-void-x.itch.io";
 itch.addEventListener("click",goToLink,false);
+
+
+//creatures, move to seperate file
+let creature = document.createElement("img");
+creature.src = "../res/images/creature.png";
+creature.style.position = "fixed";
+document.body.appendChild(creature);
+let pos = {
+  x:0, y:0
+};
+const speed = 0.4;
+const rotSpeed = 0.05;
+const minDistance = 1400;
+
+window.requestAnimationFrame(loop);
+document.onmousemove = onMouseMove;
+let lastTimestamp = 0;
+let mousePos = {
+  x:0, y:0
+};
+
+
+function onMouseMove(e)
+{
+  mousePos["x"] = e.clientX;
+  mousePos["y"] = e.clientY;
+}
+
+function loop(timestamp)
+{
+  let deltaTime = timestamp - lastTimestamp;
+
+  //update
+  let direction = {
+    "x":mousePos["x"] - pos["x"],
+    "y":mousePos["y"] - pos["y"]
+  };
+  let lenght = direction["x"] * direction["x"] + direction["y"] * direction["y"]
+  if (lenght > minDistance)
+  {
+    //normalize
+    if(lenght !== 0)
+    {
+      direction["x"] /= Math.sqrt(lenght);
+      direction["y"] /= Math.sqrt(lenght);
+    }
+
+    pos["x"] += direction["x"] * speed * deltaTime;
+    pos["y"] += direction["y"] * speed * deltaTime;
+  }
+  else
+  {
+    //rotate arround mouse
+
+    let sin = Math.sin(rotSpeed);
+    let cos = Math.cos(rotSpeed);
+
+    //translate point back to origin:
+    pos["x"] -=  mousePos["x"];
+    pos["y"] -=  mousePos["y"];
+
+    //rotate point
+    let newPosX = pos["x"] * cos - pos["y"] * sin;
+    let newPosY = pos["x"] * sin + pos["y"] * cos;
+
+    //translate point back:
+    pos["x"] = newPosX + mousePos["x"];
+    pos["y"] = newPosY + mousePos["y"];
+  }
+  
+  creature.style.left = pos["x"] + "px";
+  creature.style.top = pos["y"] + "px";
+
+  lastTimestamp = timestamp;
+  window.requestAnimationFrame(loop);
+}
