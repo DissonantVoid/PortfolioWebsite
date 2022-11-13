c92ea51f33c6dd133903fe11231e9f6c7454f7f6
Void <arandomdude75@gmail.com>
2022-11-13 20:09:32 +0000

    visual improvements

--- js/index.js
+++ js/index.js
@@ -1,6 +1,18 @@
 "use strict";
 
 window.addEventListener("load",function(){
+  //Focal name animation
+  const name = document.querySelector(".main-text > h1 > span");
+  let nameStr = name.innerText;
+  let currLetter = 0;
+  this.setInterval(function() {
+    name.innerHTML = nameStr.substr(0,currLetter) + '<span style="color:#ededed;">' + nameStr.substr(currLetter,1) + '</span>' + nameStr.substr(currLetter+1);
+    currLetter += 1;
+    if(currLetter == nameStr.length)
+      currLetter = 0;
+  },200)
+
+  //email animation 
   const email = "arandomdude75@gmail.com";
   const emailElement = document.querySelector(".email");
   emailElement.addEventListener("click",function clickHandler(e){
